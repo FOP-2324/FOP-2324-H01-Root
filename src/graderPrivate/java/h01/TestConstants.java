664acc18a1ccc336e77660b6912f12f27929b9a8
Rdeisenroth <rdeisenroth@gmail.com>
2023-11-02 19:06:24 +0100

    automatically show world when debugging tests

--- src/graderPrivate/java/h01/TestConstants.java
+++ src/graderPrivate/java/h01/TestConstants.java
@@ -1,6 +1,10 @@
 package h01;
 
 public class TestConstants {
-    public static final boolean SHOW_WORLD = false;
+    public static final boolean SHOW_WORLD = java.lang.management.ManagementFactory
+        .getRuntimeMXBean()
+        .getInputArguments()
+        .toString()
+        .contains("-agentlib:jdwp"); // true if debugger is attached
     public static final int WORLD_DELAY = 500;
 }
